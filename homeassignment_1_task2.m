ec74be75eeace2e0bd901e628b5e42e20065ebe3
hjelmare <hjelmare@users.noreply.github.com>
2016-09-28 16:19:15 +0200

    bla

--- homeassignment_1_task2.m
+++ homeassignment_1_task2.m
@@ -23,10 +23,10 @@ four = im2bw(four);
 tmp = {zero, one, two, three, four};
 
 %parameters
-nbrRepeat = 100;
+nbrRepeat = 1000;
 nbrP = 5;       % number of patterns
 maxGen = 10;    % the maximum number of generations
-switchedBits = 120;  %maximum number of switched bits
+switchedBits = 100;  %maximum number of switched bits
 [rows, cols] = size(zero);  %dimension of our figures
 N = rows*cols;  %nbr of bits
 percent = zeros(nbrP,switchedBits); %the stored percentages
